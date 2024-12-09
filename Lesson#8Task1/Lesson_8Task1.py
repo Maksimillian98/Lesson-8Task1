6c0fc881610dc59932bb0a056af2067318ed9eb7
maxstar <maksimgavura78@gmail.com>
2024-12-10 00:22:41 +0200

    Добавьте файлы проекта.

---
+++ Lesson#8Task1/Lesson_8Task1.py
@@ -0,0 +1,8 @@
+﻿N = int(input("Введите число: "))
+res = []
+for i in range(N):
+    number = int(input('Введите число: '))
+    if (number <= 10**5 and number >= 1):
+        res.append(number)
+res.reverse()
+print(res)
